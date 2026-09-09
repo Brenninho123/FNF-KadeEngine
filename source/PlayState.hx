9d4d61dd855a308f6ecda995d6068607763f0cdb
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-09-08 22:06:15 -0300

    Update conditionals for player optimization checks

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1692,7 +1692,7 @@ class PlayState extends MusicBeatState
 			//defaults if no noteStyle was found in chart
 			var noteTypeCheck:String = 'normal';
 		
-			if (PlayStateChangeables.Optimize && player == 0)
+			if ((PlayStateChangeables.Optimize || FlxG.save.data.middlescroll) && player == 0)
 				continue;
 
 			if (SONG.noteStyle == null) {
@@ -1829,7 +1829,7 @@ class PlayState extends MusicBeatState
 			babyArrow.x += 50;
 			babyArrow.x += ((FlxG.width / 2) * player);
 			
-			if (PlayStateChangeables.Optimize)
+			if (PlayStateChangeables.Optimize || (FlxG.save.data.middlescroll && player == 1))
 				babyArrow.x -= 275;
 			
 			cpuStrums.forEach(function(spr:FlxSprite)
