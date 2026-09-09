5c62bd86c079eaf359a355b98b6b8d49e77db6ea
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-09-08 21:34:11 -0300

    Refactor video handling in Debug menu case

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -254,16 +254,15 @@ class PauseSubState extends MusicBeatSubstate
 					}
 					FlxG.resetState();
 				case "Debug menu":
-					if (useVideo)
+					if (PlayState.instance.useVideo)
 					{
-					GlobalVideo.get().stop();
-					remove(videoSprite);
-					FlxG.stage.window.onFocusOut.remove(focusOut);
-					FlxG.stage.window.onFocusIn.remove(focusIn);
-					removedVideo = true;
+						GlobalVideo.get().stop();
+						PlayState.instance.remove(PlayState.instance.videoSprite);
+						FlxG.stage.window.onFocusOut.remove(PlayState.instance.focusOut);
+						FlxG.stage.window.onFocusIn.remove(PlayState.instance.focusIn);
+						PlayState.instance.removedVideo = true;
 					}
 					FlxG.switchState(new ChartingState());
-					//FlxG.stage.removeEventListener(KeyboardEvent.KEY_DOWN,handleInput);
 					#if windows
 					DiscordClient.changePresence("Chart Editor", null, null, true);
 					if (PlayState.luaModchart != null)
