c96b3e8a49e13947a09e73bb6dfe099051cbef9e
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-09-10 18:48:37 -0300

    Refactor FlxVirtualPad for better button management
    Refactor FlxVirtualPad to improve button layout and graphics handling.

--- source/mobile/FlxVirtualPad.hx
+++ source/mobile/FlxVirtualPad.hx
@@ -2,31 +2,25 @@
 package mobile;
 
 import flixel.FlxG;
-import flixel.graphics.frames.FlxTileFrames;
 import flixel.group.FlxSpriteGroup;
-import flixel.math.FlxPoint;
-import flixel.system.FlxAssets;
-import flixel.util.FlxDestroyUtil;
 import flixel.ui.FlxButton;
-import flixel.graphics.frames.FlxAtlasFrames;
-import flash.display.BitmapData;
 import flixel.graphics.FlxGraphic;
-import openfl.utils.ByteArray;
-
-/**
- * A gamepad which contains 4 directional buttons and 4 action buttons.
- * It's easy to set the callbacks and to customize the layout.
- *
- * @author Ka Wing Chin
- */
-@:keep @:bitmap("assets/preload/images/virtual-input.png")
-class GraphicVirtualInput extends BitmapData {}
-
-@:file("assets/preload/images/virtual-input.txt")
-class VirtualInputData extends #if (lime_legacy || nme) ByteArray #else ByteArrayData #end {}
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxDestroyUtil;
+import openfl.display.Shape;
+import openfl.display.BitmapData;
+import openfl.geom.Matrix;
+import openfl.text.TextField;
+import openfl.text.TextFormat;
+import openfl.text.TextFormatAlign;
 
 class FlxVirtualPad extends FlxSpriteGroup
 {
+	public static inline var BUTTON_SIZE:Int = 140;
+	public static inline var PADDING:Int = 20;
+	public static inline var GAP:Int = 4;
+
 	public var buttonA:FlxButton;
 	public var buttonB:FlxButton;
 	public var buttonC:FlxButton;
@@ -38,22 +32,9 @@ class FlxVirtualPad extends FlxSpriteGroup
 	public var buttonDown:FlxButton;
 	public var buttonPause:FlxButton;
 
-	/**
-	 * Group of directions buttons.
-	 */
 	public var dPad:FlxSpriteGroup;
-
-	/**
-	 * Group of action buttons.
-	 */
 	public var actions:FlxSpriteGroup;
 
-	/**
-	 * Create a gamepad which contains 4 directional buttons and 4 action buttons.
-	 *
-	 * @param   DPadMode     The D-Pad mode. `FULL` for example.
-	 * @param   ActionMode   The action buttons mode. `A_B_C` for example.
-	 */
 	public function new(?DPad:FlxDPadMode, ?Action:FlxActionMode)
 	{
 		super();
@@ -70,50 +51,54 @@ class FlxVirtualPad extends FlxSpriteGroup
 		actions = new FlxSpriteGroup();
 		actions.scrollFactor.set();
 
+		var s = BUTTON_SIZE;
+		var g = GAP;
+
 		switch (DPad)
 		{
 			case UP_DOWN:
-				dPad.add(add(buttonUp = createButton(0, FlxG.height - 88 * 3, 44 * 3, 45 * 3, "up")));
-				dPad.add(add(buttonDown = createButton(0, FlxG.height - 48 * 3, 44 * 3, 45 * 3, "down")));
+				dPad.add(add(buttonUp = createButton(PADDING, FlxG.height - PADDING - (s * 2 + g), s, s, "up")));
+				dPad.add(add(buttonDown = createButton(PADDING, FlxG.height - PADDING - s, s, s, "down")));
 			case LEFT_RIGHT:
-				dPad.add(add(buttonLeft = createButton(0, FlxG.height - 48 * 3, 44 * 3, 45 * 3, "left")));
-				dPad.add(add(buttonRight = createButton(42 * 3, FlxG.height - 48 * 3, 44 * 3, 45 * 3, "right")));
+				dPad.add(add(buttonLeft = createButton(PADDING, FlxG.height - PADDING - s, s, s, "left")));
+				dPad.add(add(buttonRight = createButton(PADDING + s + g, FlxG.height - PADDING - s, s, s, "right")));
 			case UP_LEFT_RIGHT:
-				dPad.add(add(buttonUp = createButton(35 * 3, FlxG.height - 74 * 3, 44 * 3, 45 * 3, "up")));
-				dPad.add(add(buttonLeft = createButton(0, FlxG.height - 48 * 3, 44 * 3, 45 * 3, "left")));
-				dPad.add(add(buttonRight = createButton(69 * 3, FlxG.height - 48 * 3, 44 * 3, 45 * 3, "right")));
+				dPad.add(add(buttonUp = createButton(PADDING + s + g, FlxG.height - PADDING - (s * 2 + g), s, s, "up")));
+				dPad.add(add(buttonLeft = createButton(PADDING, FlxG.height - PADDING - s, s, s, "left")));
+				dPad.add(add(buttonRight = createButton(PADDING + (s + g) * 2, FlxG.height - PADDING - s, s, s, "right")));
 			case FULL:
-				dPad.add(add(buttonUp = createButton(35 * 3, FlxG.height - 119 * 3, 44 * 3, 45 * 3, "up")));
-				dPad.add(add(buttonLeft = createButton(0, FlxG.height - 84 * 3, 44 * 3, 45 * 3, "left")));
-				dPad.add(add(buttonRight = createButton(69 * 3, FlxG.height - 84 * 3, 44 * 3, 45 * 3, "right")));
-				dPad.add(add(buttonDown = createButton(35 * 3, FlxG.height - 48 * 3, 44 * 3, 45 * 3, "down")));
+				dPad.add(add(buttonUp = createButton(PADDING + s + g, FlxG.height - PADDING - (s * 2 + g), s, s, "up")));
+				dPad.add(add(buttonLeft = createButton(PADDING, FlxG.height - PADDING - s, s, s, "left")));
+				dPad.add(add(buttonRight = createButton(PADDING + (s + g) * 2, FlxG.height - PADDING - s, s, s, "right")));
+				dPad.add(add(buttonDown = createButton(PADDING + s + g, FlxG.height - PADDING, s, s, "down")));
 			case RIGHT_FULL:
-				dPad.add(add(buttonUp = createButton(FlxG.width - 86 * 3, FlxG.height - 66 - 119 * 3, 44 * 3, 45 * 3, "up")));
-				dPad.add(add(buttonLeft = createButton(FlxG.width - 130 * 3, FlxG.height - 66 - 84 * 3, 44 * 3, 45 * 3, "left")));
-				dPad.add(add(buttonRight = createButton(FlxG.width - 44 * 3, FlxG.height - 66 - 84 * 3, 44 * 3, 45 * 3, "right")));
-				dPad.add(add(buttonDown = createButton(FlxG.width - 86 * 3, FlxG.height - 66 - 48 * 3, 44 * 3, 45 * 3, "down")));
-			case NONE: // do nothing
+				var baseX = FlxG.width - PADDING - (s * 3 + g * 2);
+				dPad.add(add(buttonUp = createButton(baseX + s + g, FlxG.height - PADDING - (s * 2 + g), s, s, "up")));
+				dPad.add(add(buttonLeft = createButton(baseX, FlxG.height - PADDING - s, s, s, "left")));
+				dPad.add(add(buttonRight = createButton(baseX + (s + g) * 2, FlxG.height - PADDING - s, s, s, "right")));
+				dPad.add(add(buttonDown = createButton(baseX + s + g, FlxG.height - PADDING, s, s, "down")));
+			case NONE:
 		}
 
 		switch (Action)
 		{
 			case A:
-				actions.add(add(buttonA = createButton(FlxG.width - 44 * 3, FlxG.height - 48 * 3, 44 * 3, 45 * 3, "a")));
+				actions.add(add(buttonA = createButton(FlxG.width - PADDING - s, FlxG.height - PADDING - s, s, s, "a")));
 			case A_B:
-				actions.add(add(buttonA = createButton(FlxG.width - 44 * 3, FlxG.height - 48 * 3, 44 * 3, 45 * 3, "a")));
-				actions.add(add(buttonB = createButton(FlxG.width - 86 * 3, FlxG.height - 48 * 3, 44 * 3, 45 * 3, "b")));
+				actions.add(add(buttonA = createButton(FlxG.width - PADDING - s, FlxG.height - PADDING - s, s, s, "a")));
+				actions.add(add(buttonB = createButton(FlxG.width - PADDING - (s + g) * 2, FlxG.height - PADDING - s, s, s, "b")));
 			case A_B_C:
-				actions.add(add(buttonA = createButton(FlxG.width - 128, FlxG.height - 48, 44, 45, "a")));
-				actions.add(add(buttonB = createButton(FlxG.width - 86, FlxG.height - 48, 44, 45, "b")));
-				actions.add(add(buttonC = createButton(FlxG.width - 44, FlxG.height - 48, 44, 45, "c")));
+				actions.add(add(buttonC = createButton(FlxG.width - PADDING - s, FlxG.height - PADDING - s, s, s, "c")));
+				actions.add(add(buttonB = createButton(FlxG.width - PADDING - (s + g) * 2, FlxG.height - PADDING - s, s, s, "b")));
+				actions.add(add(buttonA = createButton(FlxG.width - PADDING - (s + g) * 3, FlxG.height - PADDING - s, s, s, "a")));
 			case A_B_X_Y:
-				actions.add(add(buttonY = createButton(FlxG.width - 86, FlxG.height - 88, 44, 45, "y")));
-				actions.add(add(buttonX = createButton(FlxG.width - 44, FlxG.height - 88, 44, 45, "x")));
-				actions.add(add(buttonB = createButton(FlxG.width - 86, FlxG.height - 48, 44, 45, "b")));
-				actions.add(add(buttonA = createButton(FlxG.width - 44, FlxG.height - 48, 44, 45, "a")));
+				actions.add(add(buttonY = createButton(FlxG.width - PADDING - (s + g) * 2, FlxG.height - PADDING - (s * 2 + g), s, s, "y")));
+				actions.add(add(buttonX = createButton(FlxG.width - PADDING - s, FlxG.height - PADDING - (s * 2 + g), s, s, "x")));
+				actions.add(add(buttonB = createButton(FlxG.width - PADDING - (s + g) * 2, FlxG.height - PADDING - s, s, s, "b")));
+				actions.add(add(buttonA = createButton(FlxG.width - PADDING - s, FlxG.height - PADDING - s, s, s, "a")));
 			case STOP:
-				actions.add(add(buttonPause = createButton(FlxG.width - 44 * 3, FlxG.height - 48 - 519 * 3, 44 * 3, 45 * 3, "pause")));
-			case NONE: // do nothing
+				actions.add(add(buttonPause = createButton(FlxG.width - PADDING - s, PADDING, s, s, "pause")));
+			case NONE:
 		}
 	}
 
@@ -138,21 +123,10 @@ class FlxVirtualPad extends FlxSpriteGroup
 		buttonPause = null;
 	}
 
-	/**
-	 * @param   X          The x-position of the button.
-	 * @param   Y          The y-position of the button.
-	 * @param   Width      The width of the button.
-	 * @param   Height     The height of the button.
-	 * @param   Graphic    The image of the button. It must contains 3 frames (`NORMAL`, `HIGHLIGHT`, `PRESSED`).
-	 * @param   Callback   The callback for the button.
-	 * @return  The button
-	 */
-	public function createButton(X:Float, Y:Float, Width:Int, Height:Int, Graphic:String, ?OnClick:Void->Void):FlxButton
+	function createButton(X:Float, Y:Float, Width:Int, Height:Int, ShapeName:String):FlxButton
 	{
 		var button = new FlxButton(X, Y);
-		var frame = getVirtualInputFrames().getByName(Graphic);
-		button.frames = FlxTileFrames.fromFrame(frame, FlxPoint.get(Width, Height));
-		button.resetSizeFromFrame();
+		button.loadGraphic(buildGraphic(Width, Height, ShapeName));
 		button.solid = false;
 		button.immovable = true;
 		button.scrollFactor.set();
@@ -161,32 +135,99 @@ class FlxVirtualPad extends FlxSpriteGroup
 		button.ignoreDrawDebug = true;
 		#end
 
-		if (OnClick != null)
-			button.onDown.callback = OnClick;
+		button.onDown.callback = function()
+		{
+			FlxTween.num(1, 0.6, 0.075, {ease: FlxEase.circInOut}, function(a:Float) { button.alpha = a; });
+		};
+
+		button.onUp.callback = function()
+		{
+			FlxTween.num(button.alpha, 1, 0.1, {ease: FlxEase.circInOut}, function(a:Float) { button.alpha = a; });
+		};
+
+		button.onOut.callback = function()
+		{
+			FlxTween.num(button.alpha, 1, 0.2, {ease: FlxEase.circInOut}, function(a:Float) { button.alpha = a; });
+		};
 
 		return button;
 	}
 
-	public static function getVirtualInputFrames():FlxAtlasFrames
+	function buildGraphic(width:Int, height:Int, shapeName:String):FlxGraphic
 	{
-			#if !web
-			var bitmapData = new GraphicVirtualInput(0, 0);
-			#end
+		var bmp = new BitmapData(width, height, true, 0x00000000);
+		var base = new Shape();
 
-			/*
-			#if html5 // dirty hack for openfl/openfl#682
-			Reflect.setProperty(bitmapData, "width", 399);
-			Reflect.setProperty(bitmapData, "height", 183);
-			#end
-			*/
+		base.graphics.beginFill(0xffffff, 0.3);
+		base.graphics.drawEllipse(0, 0, width, height);
+		base.graphics.endFill();
+		bmp.draw(base);
 
-			#if !web
-			var graphic:FlxGraphic = FlxGraphic.fromBitmapData(bitmapData);
-			return FlxAtlasFrames.fromSpriteSheetPacker(graphic, Std.string(new VirtualInputData()));
-			#else
-			var graphic:FlxGraphic = FlxGraphic.fromAssetKey(Paths.image('virtual-input'));
-			return FlxAtlasFrames.fromSpriteSheetPacker(graphic, Std.string(new VirtualInputData()));
-			#end
+		switch (shapeName)
+		{
+			case "up", "down", "left", "right":
+				drawArrow(bmp, width, height, getArrowAngle(shapeName));
+			case "pause":
+				drawPause(bmp, width, height);
+			default:
+				drawLabel(bmp, width, height, shapeName);
+		}
+
+		return FlxGraphic.fromBitmapData(bmp);
+	}
+
+	function getArrowAngle(direction:String):Float
+	{
+		return switch (direction)
+		{
+			case "up": 0;
+			case "right": 90;
+			case "down": 180;
+			case "left": 270;
+			default: 0;
+		}
+	}
+
+	function drawArrow(bmp:BitmapData, width:Int, height:Int, angle:Float):Void
+	{
+		var arrow = new Shape();
+		arrow.graphics.beginFill(0xffffff, 0.9);
+		arrow.graphics.moveTo(-width * 0.2, height * 0.18);
+		arrow.graphics.lineTo(width * 0.2, height * 0.18);
+		arrow.graphics.lineTo(0, -height * 0.18);
+		arrow.graphics.lineTo(-width * 0.2, height * 0.18);
+		arrow.graphics.endFill();
+
+		var matrix = new Matrix();
+		matrix.rotate(angle * Math.PI / 180);
+		matrix.translate(width / 2, height / 2);
+		bmp.draw(arrow, matrix);
+	}
+
+	function drawPause(bmp:BitmapData, width:Int, height:Int):Void
+	{
+		var bars = new Shape();
+		bars.graphics.beginFill(0xffffff, 0.9);
+		bars.graphics.drawRect(width * 0.32, height * 0.28, width * 0.12, height * 0.44);
+		bars.graphics.drawRect(width * 0.56, height * 0.28, width * 0.12, height * 0.44);
+		bars.graphics.endFill();
+		bmp.draw(bars);
+	}
+
+	function drawLabel(bmp:BitmapData, width:Int, height:Int, letter:String):Void
+	{
+		var tf = new TextField();
+		tf.selectable = false;
+		tf.width = width;
+		tf.height = height;
+
+		var format = new TextFormat(null, Std.int(height * 0.4), 0xffffff, true);
+		format.align = TextFormatAlign.CENTER;
+		tf.defaultTextFormat = format;
+		tf.text = letter.toUpperCase();
+		tf.y = (height - tf.textHeight) / 2;
+
+		bmp.draw(tf);
 	}
 }
 
